f45b34d668f78d687a8e01915b6a0d6ae743b1ba
kyungmin-kim-teacher <81541743+kyungmin-kim-teacher@users.noreply.github.com>
2023-08-17 11:40:42 +0900

    player local 이동

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,16 +4,26 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public float _speed = 3.0f;
     // Start is called before the first frame update
     void Start()
     {
-        Managers mg = Managers.Instance;
-        Debug.Log("mg : " + mg);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.W))
+            transform.Translate(Vector3.forward * Time.deltaTime * _speed);
+    
+        if (Input.GetKey(KeyCode.S))
+            transform.Translate(Vector3.back * Time.deltaTime * _speed);
+    
+        if (Input.GetKey(KeyCode.D))
+            transform.Translate(Vector3.right * Time.deltaTime * _speed);
+    
+        if (Input.GetKey(KeyCode.A))
+            transform.Translate(Vector3.left * Time.deltaTime * _speed);
     }
 }
