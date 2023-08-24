3718e02ca060bc057d6ab23e4cffe494e3bb8edb
kyungmin-kim-teacher <81541743+kyungmin-kim-teacher@users.noreply.github.com>
2023-08-24 12:13:29 +0900

    raycast

--- Assets/Scripts/TestCollision.cs
+++ Assets/Scripts/TestCollision.cs
@@ -5,6 +5,23 @@ using UnityEngine;
 
 public class TestCollision : MonoBehaviour
 {
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Debug.DrawRay(Camera.main.transform.position, ray.direction * 100, Color.red, 1.0f);
+
+            LayerMask mask = LayerMask.GetMask("Monster") | LayerMask.GetMask("Wall");
+
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, 100, mask))
+            {
+                Debug.Log($"Raycast Camera @{hit.collider.gameObject.name}");
+            }
+        }
+    }
+
     private void OnCollisionEnter(Collision other)
     {
         Debug.Log("Collision");
