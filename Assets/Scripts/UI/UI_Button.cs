5d017262fac88e4c5badfd99b65a5c641b8e5328
kyungmin-kim-teacher <81541743+kyungmin-kim-teacher@users.noreply.github.com>
2023-09-07 11:28:08 +0900

    UI. Image drag

--- Assets/Scripts/UI/UI_Button.cs
+++ Assets/Scripts/UI/UI_Button.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 using Object = UnityEngine.Object;
 
@@ -22,17 +23,25 @@ public class UI_Button : UI_Base
         TestObject
     }
 
+    enum Images
+    {
+        ItemIcon
+    }
+
     private void Start()
     {
         Bind<Button>(typeof(Buttons));
         Bind<TextMeshProUGUI>(typeof(Texts));
         Bind<GameObject>(typeof(GameObjects));
+        Bind<Image>(typeof(Images));
         
-        //Get<TextMeshProUGUI>((int)Texts.ScoreText).text = "Bind test";
         GetTextMeshProUGUI((int)Texts.ScoreText).text = "Bind Test2";
+
+        GameObject go = GetImage((int)Images.ItemIcon).gameObject;
+        UI_EventHander ent = go.GetComponent<UI_EventHander>();
+        ent.OnDragHandler += ((PointerEventData data) => { go.transform.position = data.position; });
     }
-
-
+    
     private int _score = 0;
     
     public void OnButtonClicked()
